3c6ebff7eef76eb2b3937c695aadf952dddd5436
felix <okekefelix1@gmail.com>
2024-07-03 05:43:10 +0100

    adds suspense boundary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import HomePage from "@/features/home";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="p-3 sm:p-6">
       <h1 className="text-3xl text-center mb-3 font-bold">SPA APP</h1>
-      <HomePage />
+      <Suspense>
+        <HomePage />
+      </Suspense>
     </main>
   );
 }
